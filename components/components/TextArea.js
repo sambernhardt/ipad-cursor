9adee2c7cf218545d926868a7be80ef2479dd851
Samuel Bernhardt <sdbernhardt@gmail.com>
2020-05-09 10:33:42 -0400

    Removed local show cursor logic

--- components/components/TextArea.js
+++ components/components/TextArea.js
@@ -12,11 +12,6 @@ const Container = WithHover(styled.textarea`
   color: ${({ theme }) => theme.colors.body};
   border: none;
   margin-bottom: 24px;
-  /* ${({ showingCursor }) => !showingCursor && `
-    cursor: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAFBhaW50Lk5FVCB2My41LjbQg61aAAAADUlEQVQYV2P4//8/IwAI/QL/+TZZdwAAAABJRU5ErkJggg=='),
-      url(cursor.png),
-      none;
-  `} */
   resize: none; 
   &:focus {
       outline: none;
