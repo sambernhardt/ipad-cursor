7c70a449988db1e94073c09efc3adbda2ed805db
Samuel Bernhardt <sdbernhardt@gmail.com>
2020-05-09 11:23:44 -0400

    Pre-cursor refactor

--- cursor/WithHover.js
+++ cursor/WithHover.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext } from 'react';
 import CursorContext from './Context';
 
 import { getRelativePosition } from './utils';
